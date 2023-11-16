24bd99b6b294fbb92c94652bfa9a5cd8dd7183ab
Michael Mwai <michaeldmwai@gmail.com>
2023-11-16 06:57:37 -0800

    Compile subtract.c

--- subtract.c
+++ subtract.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
-#include <unistd.h>
 
-int main(void) {
-    // Declare variables to store the numbers
-    int num1, num2;
+/**
+ * main - Entry point of the program.
+ *
+ * This function takes user input, performs subtraction,  displays result.
+ *
+ * Return: 0 if the program executed successfully.
+ */
+int main(void)
+{
+	/* Declare variables to store the numbers */
+	int num1, num2;
 
-    // Prompt the user to enter the first number
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+	/* Prompt the user to enter the first number */
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
 
-    // Prompt the user to enter the second number
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+	/* Prompt the user to enter the second number */
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
 
-    // Calculate the difference of the two numbers
-    int difference = num1 - num2;
+	/* Calculate the difference of the two numbers */
+	int difference = num1 - num2;
 
-    // Display the result
-    printf("The difference of %d and %d is: %d\n", num1, num2, difference);
+	/* Display the result */
+	printf("The difference of %d and %d is: %d\n", num1, num2, difference);
 
-    return 0;
+	/* Return 0 to indicate successful execution */
+	return (0);
 }
 
